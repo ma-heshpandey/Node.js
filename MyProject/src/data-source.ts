1d0a1b4b18a54b7082446cbb38647657c3254b33
Mahesh Pandey <mahesh.pandey@itonics.de>
2022-06-27 18:09:47 +0545

    Configuring .env file

--- MyProject/src/data-source.ts
+++ MyProject/src/data-source.ts
@@ -2,14 +2,16 @@ import "reflect-metadata"
 import { DataSource, Migration } from "typeorm"
 import { User } from "./entity/User"
 import {Book} from "./entity/Book"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "postgres",
-    password: "mahesh",
+    username: process.env.user,
+    password: process.env.password ,
     database: "MyProject",
     synchronize: true,
     logging: false,
--- MyProject/src/index.ts
+++ MyProject/src/index.ts
@@ -1,18 +1,21 @@
 import express  from 'express'
 import { AppDataSource } from "./data-source"
 import { bookRouter } from './router/bookRouter'
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const bodyParser = require("body-parser")
 
-const Port = 8000
+//const Port = 8000
 
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use('/book', bookRouter)
+console.log(process.env.user)
 
-app.listen(Port,()=>{
+app.listen(process.env.PORT ,()=>{
     console.log("Express is connected.")
 
     AppDataSource.initialize().then(async () => {
